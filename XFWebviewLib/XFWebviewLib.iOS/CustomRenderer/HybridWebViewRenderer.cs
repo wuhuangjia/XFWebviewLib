70edd4ba86a00f6a6cac22510246e9237de8bc61
wuhuangjia <wuhuangjia@msn.com>
2017-11-18 16:23:58 +0800

    HybridWebViewRenderer

---
+++ XFWebviewLib/XFWebviewLib.iOS/CustomRenderer/HybridWebViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WebKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFWebviewLib.CustomRenderer;
+using XFWebviewLib.iOS.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace XFWebviewLib.iOS.CustomRenderer
+{
+    class HybridWebViewRenderer : ViewRenderer<HybridWebView, WKWebView>, IWKScriptMessageHandler
+    {
+        const string JavaScriptFunction = "function invokeCSharpAction(data){window.webkit.messageHandlers.invokeAction.postMessage(data);}";
+        WKUserContentController userController;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                userController = new WKUserContentController();
+                var script = new WKUserScript(new NSString(JavaScriptFunction), WKUserScriptInjectionTime.AtDocumentEnd, false);
+                userController.AddUserScript(script);
+                userController.AddScriptMessageHandler(this, "invokeAction");
+
+                var config = new WKWebViewConfiguration { UserContentController = userController };
+                var webView = new WKWebView(Frame, config);
+                SetNativeControl(webView);
+            }
+            if (e.OldElement != null)
+            {
+                userController.RemoveAllUserScripts();
+                userController.RemoveScriptMessageHandler("invokeAction");
+                var hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+            if (e.NewElement != null)
+            {
+                string fileName = Path.Combine(NSBundle.MainBundle.BundlePath, string.Format("Content/{0}", Element.Uri));
+                Control.LoadRequest(new NSUrlRequest(new NSUrl(fileName, false)));
+            }
+        }
+
+        public void DidReceiveScriptMessage(WKUserContentController userContentController, WKScriptMessage message)
+        {
+            Element.InvokeAction(message.Body.ToString());
+        }
+    }
+}
